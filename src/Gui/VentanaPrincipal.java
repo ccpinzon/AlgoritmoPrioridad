6e1668ed6fb5e3deef2b5a3cf07b13c15412c2e6
ccpinzon <rdz.cristhian@gmail.com>
2016-10-10 21:18:07 -0500

    Se creo la ventana principal

---
+++ src/Gui/VentanaPrincipal.java
@@ -0,0 +1,31 @@
+package Gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by cristhian on 10/10/16.
+ */
+public class VentanaPrincipal extends JFrame {
+
+    public VentanaPrincipal()  {
+        this.setSize(600,400);
+        beginComponets();
+        addComponents();
+    }
+
+    private void beginComponets() {
+
+
+    }
+
+    private void addComponents() {
+
+    }
+
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setVisible(true);
+    }
+
+}
